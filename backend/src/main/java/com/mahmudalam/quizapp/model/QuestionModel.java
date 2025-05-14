1bda568a12a3ff51d3d043ac39f099051cf7d74b
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-15 02:16:59 +0600

    create question model as like my postgresql question table

---
+++ backend/src/main/java/com/mahmudalam/quizapp/model/QuestionModel.java
@@ -0,0 +1,24 @@
+package com.mahmudalam.quizapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity()
+@Table(name="question")
+public class QuestionModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String category;
+    private String difficultyLevel;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String answer;
+}
