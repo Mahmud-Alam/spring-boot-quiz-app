08ac22e4552a01ea2e8e6ed339ea2b6b4a466c09
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-16 00:24:07 +0600

    connect Service layer to Dao layer and fetch data from db

--- backend/src/main/java/com/mahmudalam/quizapp/service/QuestionService.java
+++ backend/src/main/java/com/mahmudalam/quizapp/service/QuestionService.java
@@ -1,7 +1,19 @@
 package com.mahmudalam.quizapp.service;
 
+import com.mahmudalam.quizapp.dao.QuestionDao;
+import com.mahmudalam.quizapp.model.QuestionModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<QuestionModel> getAllQuestions() {
+        return questionDao.findAll();
+    }
 }
