96a2ac538de1ff54cb331863817e4f35516beb09
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-16 00:24:51 +0600

    connect controller with service layer and fetch data from db

--- backend/src/main/java/com/mahmudalam/quizapp/controller/QuestionController.java
+++ backend/src/main/java/com/mahmudalam/quizapp/controller/QuestionController.java
@@ -1,15 +1,25 @@
 package com.mahmudalam.quizapp.controller;
 
+import com.mahmudalam.quizapp.model.QuestionModel;
+import com.mahmudalam.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("question")
+@CrossOrigin(origins = "http://localhost:5173")
 public class QuestionController {
 
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
-        return "Hello World!";
+    public List<QuestionModel> getAllQuestions(){
+        return questionService.getAllQuestions();
     }
 }
